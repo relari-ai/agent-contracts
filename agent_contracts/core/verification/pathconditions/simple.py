3be590ff11fdc9b391965448799e4f7b4bae991e
Pasquale Antonante <p.antonante@gmail.com>
2025-02-27 16:58:35 -0800

    Single-step Pathcondition

--- agent_contracts/core/verification/pathconditions/simple.py
+++ agent_contracts/core/verification/pathconditions/simple.py
@@ -1,17 +1,33 @@
-from pydantic import Field
+from openai import AsyncOpenAI
+from pydantic import BaseModel, Field
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+from agent_contracts.core.prompts.provider import PromptProvider
+from agent_contracts.core.verification.base import VerificationResults
+
 
 from agent_contracts.core.datatypes.specifications.requirement import (
     BasePathcondition,
 )
 from agent_contracts.core.datatypes.verification import ExecutionPath
-from agent_contracts.core.verification.base import VerificationResults
+# from .nl_requirement_checker import NLRequirementChecker
 
-from .nl_requirement_checker import NLRequirementChecker
+
+
+class _VerifyResult(BaseModel):
+    explanation: str
+    satisfied: bool
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+async def _completion(client: AsyncOpenAI, **kwargs) -> BaseModel:
+    response = await client.beta.chat.completions.parse(**kwargs)
+    return response.choices[0].message.parsed
 
 
 class Pathcondition(BasePathcondition):
     requirement: str = Field(...)
-
+    
     def __init__(self, requirement: str, **kwargs):
         if "name" not in kwargs:
             kwargs["name"] = requirement
@@ -22,15 +38,41 @@ class Pathcondition(BasePathcondition):
         return self.requirement
 
     async def check(self, exec_path: ExecutionPath) -> VerificationResults:
-        checker = NLRequirementChecker(self.requirement)
-        early_termination = False
-        await checker.init(exec_path)
-        for state in exec_path.states:
-            if early_termination:
-                break
-            for action in state.actions:
-                update = await checker.step(state, action)
-                early_termination = update.early_termination
-                if early_termination:
-                    break
-        return await checker.verify()
+        client = AsyncOpenAI()
+        prompt = PromptProvider.get_prompt("verification/pathcondition/single/")
+        msgs = prompt.render(requirement=self.requirement, exec_path=exec_path)
+        result = await _completion(
+            client=client,
+            model="o3-mini",
+            messages=msgs,
+            response_format=_VerifyResult,
+        )
+        return VerificationResults(
+            satisfied=result.satisfied, explanation=result.explanation
+        )
+
+# class Pathcondition(BasePathcondition):
+#     requirement: str = Field(...)
+
+#     def __init__(self, requirement: str, **kwargs):
+#         if "name" not in kwargs:
+#             kwargs["name"] = requirement
+#         super().__init__(requirement=requirement, **kwargs)
+
+#     @property
+#     def name(self) -> str:
+#         return self.requirement
+
+#     async def check(self, exec_path: ExecutionPath) -> VerificationResults:
+#         checker = NLRequirementChecker(self.requirement)
+#         early_termination = False
+#         await checker.init(exec_path)
+#         for state in exec_path.states:
+#             if early_termination:
+#                 break
+#             for action in state.actions:
+#                 update = await checker.step(state, action)
+#                 early_termination = update.early_termination
+#                 if early_termination:
+#                     break
+#         return await checker.verify()
